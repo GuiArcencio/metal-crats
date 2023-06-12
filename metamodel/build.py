9265d6f40542651a714b0b94cbd55ec67ccc2739
Guilherme Arcencio <guiarcencio@hotmail.com>
2023-06-12 12:27:51 -0300

    Adds normalizer to metamodels

--- metamodel/build.py
+++ metamodel/build.py
@@ -1,5 +1,6 @@
 from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import VarianceThreshold
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
@@ -18,15 +19,24 @@ def build_metamodel(option="1nn", random_state=None):
 
     feature_selector = VarianceThreshold((1e-5)**2)
     if option == "1nn":
-        model = KNeighborsClassifier(n_neighbors=1)
+        model = Pipeline([
+            ("normalizer", StandardScaler()),
+            ("model", KNeighborsClassifier(n_neighbors=1))
+        ])
     elif option == "5nn":
-        model = KNeighborsClassifier(n_neighbors=5)
+        model = Pipeline([
+            ("normalizer", StandardScaler()),
+            ("model", KNeighborsClassifier(n_neighbors=5))
+        ])
     elif option == "nb":
         model = GaussianNB()
     elif option == "rf":
         model = RandomForestClassifier(random_state=random_state)
     elif option == "svm":
-        model = SVC()
+        model = Pipeline([
+            ("normalizer", StandardScaler()),
+            ("model", SVC())
+        ])
 
     final_model = Pipeline([
         ("feature_selection", feature_selector),
