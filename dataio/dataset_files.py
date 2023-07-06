fc44e0ccf014a1aa877b9f327e48e401440ed0e7
Guilherme Arcencio <guiarcencio@hotmail.com>
2023-07-06 15:52:51 -0300

    Starts adapting to both classification and regression

--- dataio/dataset_files.py
+++ dataio/dataset_files.py
@@ -4,34 +4,45 @@ import os
 
 from tsml.datasets import load_from_ts_file
 
-def load_dataset(name, path="./assets/datasets"):
+def load_dataset(name, regression=True, path=None):
+    if path is None:
+        path = f"./assets/{'regression' if regression else 'classification'}/datasets"
+
     X_train, y_train = load_from_ts_file(f"{path}/{name}/{name}_TRAIN.ts")
     X_test, y_test = load_from_ts_file(f"{path}/{name}/{name}_TEST.ts")
-    y_train = y_train.astype(float)
-    y_test = y_test.astype(float)
+    if regression:
+        y_train = y_train.astype(float)
+        y_test = y_test.astype(float)
 
     X = np.concatenate([X_train, X_test], axis=0)
     y = np.concatenate([y_train, y_test])
 
     return X, y
 
-def list_datasets(path="./assets/datasets"):
+def list_datasets(problem_type="regression", path=None):
+    if path is None:
+        path = f"./assets/{problem_type}/datasets"
+
     datasets = os.listdir(path)
     return datasets
 
-def load_metadataset(X_filename="efficient.csv", path="./assets/metadataset"):
-    X = pd.read_csv(f"{path}/{X_filename}", index_col="name")
-    y = pd.read_csv("./assets/best_regressors.csv")
+def load_metadataset(features="efficient", problem_type="regression", path=None):
+    if path is None:
+        path = f"./assets/{problem_type}"
+
+    X = pd.read_csv(f"{path}/metadatasets/{features}.csv", index_col="name")
+    y = pd.read_csv("{path}/best_estimators.csv")
 
     X = X.sort_values(by="name")
     y = y.sort_values(by="dataset")
-    y = y["best_regressor"].to_numpy()
+    y = y["best_estimator"].to_numpy()
 
     # TODO: remove this line when results are available
-    X = X.drop("AustraliaRainfall")
+    if problem_type == "regression":
+        X = X.drop("AustraliaRainfall")
 
     return X, y
 
-def load_rmses(path="./assets/rmse.csv"):
+def load_rmses(path="./assets/regression/rmse.csv"):
     rmse = pd.read_csv(path, index_col="regressor")
     return rmse
