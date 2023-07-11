ea80b5e14c18659f9075066bb14aa07d1b724a32
Guilherme Arcencio <guiarcencio@hotmail.com>
2023-07-11 14:44:21 -0300

    Updates download script

--- download_datasets.sh
+++ download_datasets.sh
@@ -3,9 +3,16 @@
 mkdir -p tmp
 pushd tmp
     # Regression
-    wget -c https://github.com/GuiArcencio/metal-crats-files/raw/main/regression_datasets.tar.gz
+    gdown 1Rd_Qm5jG9jTSXBK0VqS7iXrjSBIxsaFC
     tar -xzvf regression_datasets.tar.gz
+    mkdir -p ../assets/regression/datasets
     mv regression_datasets/* ../assets/regression/datasets
+
+    # Classification
+    gdown 1k8m1BU7XPE5OHzyD_evrS8gYsFMcuPci
+    tar -xzvf classification_datasets.tar.gz
+    mkdir -p ../assets/classification/datasets
+    mv classification_datasets/* ../assets/classification/datasets
 popd
 
 rm -rf tmp
